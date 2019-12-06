22cc687f9312e50dca5c168675400025b8fbc6dc
TheoHappy <teodore473@gmail.com>
2019-12-06 13:29:09 +0200

    Add 2 classes

--- src/ModifyingTheInternalDataRepresentationOfAClass/Time2.java
+++ src/ModifyingTheInternalDataRepresentationOfAClass/Time2.java
@@ -6,7 +6,7 @@ public class Time2
     private int hour; // 0 - 23
     private int minute; // 0 - 59
     private int second; // 0 - 59
-   // private int secondsSinceMidnight;
+    private int secondsSinceMidnight;
 
     public Time2()
     {
